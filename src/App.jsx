966b855e1ed439204188c8e65aa3f64b0854fb1a
Parthiv <bhonagirip23@gmail.com>
2026-01-05 22:36:12 -0500

    tutorial done, about to delete everything and remake everything for the web app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
+import { useState, useEffect } from 'react'
+
 const Card = ({ title }) => {
+    const [count, setCount] = useState(0);
+    const [ hasLiked, setHasLiked ] = useState(false); //makes more sense to have this here because each card has a different hasLiked state
+
+    useEffect(() => {
+        console.log(`${title} has been liked: ${count}`);
+    }, [hasLiked]);
+
     return (
-        <div className={'card'} style={{
-            border: '1px solid #4b5362',
-            padding: '20px',
-            margin: '10px',
-            backgroundColor: '#31363f',
-            borderRadius: '10px',
-            minHeight: '100px'
-        }}>
-            <h2>{ title }</h2>
+        <div className={'card'} onClick={() => setCount((prevState) => prevState + 1)}>
+            <h2>{ title } <br /> { count ? count : null}</h2>
+
+            <button onClick={() => setHasLiked(!hasLiked)}>
+                {hasLiked ? 'Liked' : 'Like'}
+            </button>
         </div>
     )
-}
+} //changing the output in like 11 depending on the state is important for each card using the if else colon operator
 
 const App = () => {
+
     return(
-        <div className={'card-container'}>
-            <Card title={'Star Wars'}/>
-            <Card title={'Avatar'}/>
-            <Card title={'The Lion King'}/>
+        <div className={ 'card-container' }>
+            <Card title={ 'Star Wars' }/>
+            <Card title={ 'Avatar' }/>
+            <Card title={ 'The Lion King' }/>
         </div>
     )
 }
